99fb25577f938201892d55a48a034280fb8adaec
GANIDU2413 <ganidusahan@gmail.com>
2024-01-29 09:21:07 +0530

    0921290124

--- src/DP/FacadeDP/Demo.java
+++ src/DP/FacadeDP/Demo.java
@@ -3,9 +3,7 @@ package DP.FacadeDP;
 public class Demo {
     public static void main(String[] args) {
         MultimediaFacade mmf = new MultimediaFacade();
-        mmf.playAudio("Chamara weerasinghe");
-        mmf.playVideo("nirosha thalagala with U dancer");
-        mmf.projectOnScreen();
+        mmf.PlayVideo("videos playing......","audios playing......");
     }
 
 }
--- src/DP/FacadeDP/MultimediaFacade.java
+++ src/DP/FacadeDP/MultimediaFacade.java
@@ -1,14 +1,14 @@
 package DP.FacadeDP;
 
 public class MultimediaFacade {
-    void playAudio(String audio){
-        new AudioPlayer().play(audio);
-    }
-    void playVideo(String video){
-        new VideoPlayer().play(video);
-    }
-    void projectOnScreen(){
-        new Projector().projectOnScreen();
+    private final AudioPlayer ap = new AudioPlayer();
+    private final VideoPlayer vp = new VideoPlayer();
+    private final Projector pj = new Projector();
+
+    public void PlayVideo(String video,String audio){
+        ap.play(audio);
+        vp.play(video);
+        pj.projectOnScreen();
     }
 
 }
